bfa3c8e5580a933a35882329b3439ab97a8c3835
junliu1 <junliu1@ut.utm.edu>
2018-04-03 15:27:42 -0500

    First commit: adding project main.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 int sum(int n);
+int product(int p);
+
 int main(){
   cout << "Hello World!" << endl;
   int n;
@@ -11,12 +13,21 @@ int main(){
   cin >> n;
   int s = sum(n);
   cout << "Sum: " << s << endl;
+  
+  int p = product(n);
+  cout << "Product: " << p << endl;
 }
 int sum(int n){
-  int s;
-  for(int i = 0; i <= n; i ++ ){
+  int s = 0;
+  for(int i = 1; i <= n; i ++){
     s = s + i;
   }
   return s;
-
+}
+int product(int p){
+  int s = 1;
+  for(int i = 1; i <= p; i ++){
+    s = s * i;  
+  }
+  return s;
 }
